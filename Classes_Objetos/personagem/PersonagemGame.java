4cc3bb523236748ef72ca4a0d209fb2de447be81
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-10 17:15:05 +0000

    Commit classes

---
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -0,0 +1,11 @@
+public class PersonagemGame {
+
+    public int saudeAtual;
+    public String nome;
+
+    public PersonagemGame(){
+        this.nome = null;
+        this.saudeAtual=0;
+    }
+    
+}
