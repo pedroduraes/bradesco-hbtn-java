b937c07f5b125368065f793ae3a3a365bf134f09
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-10 17:57:24 +0000

    3. Status do Personagem

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,47 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame() {
+        this.nome = null;
+        this.saudeAtual = 0;
+        this.status = null;
+    }
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public void tomarDano(int quantidadeDeDano) {
+        this.setSaudeAtual(this.getSaudeAtual() - quantidadeDeDano);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        this.setSaudeAtual(this.getSaudeAtual() + quantidadeDeCura);
+    }
+
+}
