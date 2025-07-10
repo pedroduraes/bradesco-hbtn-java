ed3f7ebcb5f5a4e268a508b039ca366817e15e24
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-10 18:28:03 +0000

    4. Consistência

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,59 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.nome = nome;
+        this.setSaudeAtual(saudeAtual);
+    }
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && nome.trim().length() > 0){
+            this.nome = nome;
+        }
+      
+    }
+
+    
+    public void tomarDano(int quantidadeDeDano) {
+        if (this.getSaudeAtual() - quantidadeDeDano <= 0) {
+            this.setSaudeAtual(0);
+        } else {
+            this.setSaudeAtual(this.getSaudeAtual() - quantidadeDeDano);
+        }
+        
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if (this.getSaudeAtual() + quantidadeDeCura >= 100) {
+            this.setSaudeAtual(100);
+        } else {
+            this.setSaudeAtual(this.getSaudeAtual() + quantidadeDeCura);
+        }
+        
+    }
+
+}
