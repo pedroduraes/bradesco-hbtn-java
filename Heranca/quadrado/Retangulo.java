211729c261ad007d8828bb9ca8f6ba85cad37405
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-15 20:52:50 +0000

    5. Quadrado

---
+++ Heranca/quadrado/Retangulo.java
@@ -0,0 +1,35 @@
+public class Retangulo extends FormaGeometrica{
+    
+    protected double largura;
+    protected double altura;
+
+    public double getLargura() {
+        return this.largura;
+    }
+    public void setLargura(double largura) {
+        if (largura < 0) {
+            throw new java.lang.IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            throw new java.lang.IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+
+    @Override
+    public double area(){
+        // (b * h) / 2
+        return (this.largura * this.altura);
+    }
+
+    public String toString() {
+        return String.format("[Retangulo] %.2f / %.2f", this.largura, this.altura);
+    }
+}
