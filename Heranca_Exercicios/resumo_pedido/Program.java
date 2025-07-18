278a43c6dac9435d1c529913f15b7cb3010743ca
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-18 20:27:30 +0000

    1. Resumo Pedido v3

--- Heranca_Exercicios/resumo_pedido/Program.java
+++ Heranca_Exercicios/resumo_pedido/Program.java
@@ -9,7 +9,7 @@ public class Program {
                 produtos.Livro livro3 = new produtos.Livro("Mindset Milionario", 2021, "Brasil", 31.70, 272, "José Roberto Marques", 1);
 
                 produtos.Dvd dvd1 = new produtos.Dvd("Anjos da Noite 5 - Guerras de Sangue", 2016, "Estados Unidos", 16.90, "Anna Foerster", "Ação", 91);
-                produtos.Dvd dvd2 = new produtos.Dvd("Annabelle 2 - A Criacao do Mal", 2017, "Brasil", 39.90, "Jame Wan e Peter Safran", "Terror", 109);
+                produtos.Dvd dvd2 = new produtos.Dvd("xxxAnnabelle 2 - A Criacao do Mal", 2017, "Brasil", 39.90, "Jame Wan e Peter Safran", "Terror", 109);
 
                 // Pedido 1
                 ItemPedido[] itensPedido1 = new ItemPedido[] {
