3045aca596520070b1ea7dae0c2e88149fb08b9a
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-25 22:47:16 +0000

    1. Armazéns específicos02

--- Generics_Exercises/armazem_especifico/Armazem.java
+++ Generics_Exercises/armazem_especifico/Armazem.java
@@ -1,9 +1,15 @@
-abstract class Armazem<T> implements Armazenavel<T>{
+abstract class Armazem<T> implements Armazenavel<T> {
+    /*
+     * An abstract class in Java is a class that cannot be instantiated directly,
+     * meaning you cannot create objects of an abstract class. It serves as a
+     * blueprint or a base class for other classes to extend. Abstract classes are
+     * declared using the abstract keyword.
+     */
 
     @Override
     public abstract void adicionarAoInventario(String nome, T valor);
 
     @Override
     public abstract T obterDoInventario(String nome);
-    
+
 }
