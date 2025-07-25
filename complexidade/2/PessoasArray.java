5de8d701d1f3815c0f4995cfb78d9eb95c10f572
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-25 20:57:41 +0000

    2. Complexidade O(log(n)) – Busca Binária

---
+++ complexidade/2/PessoasArray.java
@@ -0,0 +1,49 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de buscaBinaria
+    public void buscaBinaria(String nome){
+        boolean encontrado = false;
+        System.out.printf("Procurando pelo nome: \"%s\"\n", nome);
+
+        int low = 0;
+        int high = nomes.length - 1;
+        int mid=0;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            int comparison = nome.compareTo(nomes[mid]); // Compare strings
+            System.out.printf("Passando pelo indice:%d\n", mid);
+            if (comparison == 0) { // Target found
+                encontrado = true;
+                break;
+            } else if (comparison < 0) { // Target is smaller, search left half
+                high = mid - 1;
+            } else { // Target is larger, search right half
+                low = mid + 1;
+            }
+        }
+        if (!encontrado) {
+            throw new IllegalArgumentException(String.format("O nome %s não se encontra no array de nomes", nome));
+        } else {
+            System.out.printf("Nome pesquisado é %s que está na posição %d\n", nome, mid);
+        }
+
+
+    }
+}
