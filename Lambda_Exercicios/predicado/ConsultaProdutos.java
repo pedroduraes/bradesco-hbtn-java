f474324da55afbe71436db7eaa8644406d42b50b
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-30 21:31:26 +0000

    1. Consultar Produtos com Predicado

---
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ConsultaProdutos {
+
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterioFiltro) {
+
+        return produtos.stream().filter((p) -> criterioFiltro.test(p)).collect(Collectors.toList());
+        /*
+         * List<Produto> items = new ArrayList<>();
+         * for (Produto p : produtos) {
+         * if (criterioFiltro.test(p)) {
+         * items.add(p);
+         * }
+         * }
+         * return items;
+         * 
+         * 
+         */
+
+    }
+}
---
+++ Lambda_Exercicios/predicado/Produto.java
@@ -0,0 +1,53 @@
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private double peso;
+    private int quantidadeEmEstoque;
+    private TiposProduto tipo;
+
+    
+    public Produto(String nome, double preco, double peso, int quantidadeEmEstoque, TiposProduto tipo) {
+        this.nome = nome;
+        this.preco = preco;
+        this.peso = peso;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.tipo = tipo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+    public TiposProduto getTipo() {
+        return tipo;
+    }
+    public void setTipo(TiposProduto tipo) {
+        this.tipo = tipo;
+    }
+    @Override
+    public String toString() {
+        return String.format("%s %.6f %.6f %d %s", this.getNome(), this.getPreco(), this.getPeso(), this.getQuantidadeEmEstoque(), this.getTipo());
+    }
+
+}
