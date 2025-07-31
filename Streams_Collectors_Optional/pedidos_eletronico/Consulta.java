7745ce483e03ed9efc35d1da67ebde073e27c9d1
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-07-31 16:36:52 +0000

    3. Relatório Pedidos com Eletrônicos

--- Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -11,41 +11,46 @@ public class Consulta {
 
     public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
         /*
-         * Ordene a lista de produtos por preco com order reversa e obtenha o primeiro produto.
+         * Ordene a lista de produtos por preco com order reversa e obtenha o primeiro
+         * produto.
          * Utilize Comparator.comparing com reversed
          */
-        
+
         List<Produto> clone = produtos.stream().collect(Collectors.toList());
-        
-        
 
-
-         Collections.sort(clone, new Comparator<Produto>() {
+        Collections.sort(clone, new Comparator<Produto>() {
             public int compare(Produto o1, Produto o2) {
                 return Double.compare(o1.getPreco(), o2.getPreco());
-                
+
             }
-         });
-         return clone.get(clone.size()-1);
+        });
+        return clone.get(clone.size() - 1);
     }
 
     public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
         /*
-         * Inclua o método estático obterProdutosPorPrecoMinimo a classe Consulta que recebe uma lista de produtos e um preço mínimo. 
-         * Esse método deve retornar os produtos que possuem preço maior ou igual ao preço mínimo passado por parâmetro.
-
+         * Inclua o método estático obterProdutosPorPrecoMinimo a classe Consulta que
+         * recebe uma lista de produtos e um preço mínimo.
+         * Esse método deve retornar os produtos que possuem preço maior ou igual ao
+         * preço mínimo passado por parâmetro.
+         * 
          */
-        
-         return produtos.stream().filter((p) -> p.getPreco() >= precoMinimo).collect(Collectors.toList());
+
+        return produtos.stream().filter((p) -> p.getPreco() >= precoMinimo).collect(Collectors.toList());
 
     }
 
-    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> produtos) {
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
         /*
-         * Inclua o método estático obterPedidosComEletronicos a classe Consulta que recebe uma lista de pedidos 
-         * e retorna apenas os pedidos que possuem produtos que sejam da categoria Eletronico.
+         * Inclua o método estático obterPedidosComEletronicos a classe Consulta que
+         * recebe uma lista de pedidos
+         * e retorna apenas os pedidos que possuem produtos que sejam da categoria
+         * Eletronico.
          */
-        return produtos.stream().anyMatch((p) -> p.getProdutos())
+        return pedidos
+                .stream().filter((p) -> p.getProdutos().stream()
+                        .filter((x) -> x.getCategoria() == CategoriaProduto.ELETRONICO).count() > 0)
+                .collect(Collectors.toList());
     }
-    
+
 }
