b5419a08ea66cef94b3496538a0b67b372c6aed2
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-01 18:02:30 +0000

    0. Sincronização com Threads

---
+++ threads/0/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+
+    /*
+     * Você deve criar uma classe chamada Principal que instancie a classe Contador, criando duas threads para incrementar o contador.
+     * Inicie as Threads e imprima o valor final do contador.
+     * 
+
+     * Inicie as Threads e imprima o valor final do contador.
+     * Valor final do contador: 2000
+     * 
+     */
+
+     public static void main(String[] args) {
+        Contador c = new Contador();
+        ThreadContador t = new ThreadContador();
+     }
+
+}
