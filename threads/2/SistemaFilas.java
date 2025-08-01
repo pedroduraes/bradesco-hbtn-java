06147ae69a1182326e66256d208fc5169ba9bc39
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-01 20:07:21 +0000

    2. Simulação de um Sistema de Filas com Produtores e Consumidores

--- threads/2/SistemaFilas.java
+++ threads/2/SistemaFilas.java
@@ -4,15 +4,23 @@ public class SistemaFilas {
     public static void main(String[] args) {
         
         Fila fila = new Fila(10);
-        Produtor p = new Produtor(fila);
-        Consumidor c = new Consumidor(fila);
+        
+        Produtor p1 = new Produtor(fila);
+        Produtor p2 = new Produtor(fila);
+        
+        Consumidor c1 = new Consumidor(fila);
+        Consumidor c2 = new Consumidor(fila);
 
-        p.start();
-        c.start();
+        p1.start();
+        p2.start();
+        c1.start();
+        c2.start();
 
         try {
-            p.join();
-            c.join();
+            p1.join();
+            p2.join();
+            c1.join();
+            c2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
