c801e6e3540f7822827b31ab578a59a827261648
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-01 17:35:05 +0000

    3. Média salarial por cargo

--- Streams_Collector_Optional_Exercicios/media_salario_por_cargo/ConsultaPessoas.java
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -5,19 +5,6 @@ import java.util.stream.Collectors;
 public class ConsultaPessoas {
 
     /*
-     * um Map<String, Map<Integer, Long>> que contém o nome do cargo como chave do primeiro Map
-        e um Map que possua a idade e quantidade de pessoas daquela idade e daquele cargo.
-
-     DICA: Utilizar Collectors.groupingBy dentro de Collectors.groupingBy
-
-     */
-    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
-
-        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
-
-    }
-
-    /*
      * um Map<String, Double> que contém o nome do cargo como chave do primeiro Map
         e um Double que possua o salário médio daquele cargo.
 
