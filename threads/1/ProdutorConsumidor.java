7a0f22a9b4e6dbf6c4d72bd1705c2add64f263de
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-01 18:41:46 +0000

    1. Notificações em Java

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,13 @@
+public class ProdutorConsumidor {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+
+        //Você deve criar as classes Buffer, Produtor, Consumidor e ProdutorConsumidor 
+        //e na classe ProdutorConsumidor criar e iniciar as threads do produtor e consumidor
+
+        // Criar e iniciar as threads do produtor e consumidor
+        Produtor p = new Produtor();
+
+
+    }
+}
