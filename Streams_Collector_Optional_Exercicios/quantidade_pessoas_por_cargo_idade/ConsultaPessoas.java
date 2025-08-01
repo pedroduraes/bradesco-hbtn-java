41445cf5ff0914b1473a54bb70cb75849902e038
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-01 17:22:45 +0000

    2. Contagem de pessoas por idade e cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/ConsultaPessoas.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    /*
+     * um Map<String, Map<Integer, Long>> que contém o nome do cargo como chave do primeiro Map
+        e um Map que possua a idade e quantidade de pessoas daquela idade e daquele cargo.
+
+     DICA: Utilizar Collectors.groupingBy dentro de Collectors.groupingBy
+
+     */
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+
+    }
+
+}
